8479b4fcd54b05fb180d40437a350c32e3632b6f
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-08-17 15:50:52 -0600

    took off the refactoring code

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -2,40 +2,18 @@ require 'pry'
 
 ages = [24, 30, 18, 20, 41]
 
-
-
-# def std_dev(ages)
-#   sum = ages.sum.to_f
-#   total = ages.count.to_f
-#   average = sum/total
-#
-#   x = ages.map do |age|
-#     age - average
-#   end
-#
-#   rounded_x = x.map do |num|
-#     num.round(2)
-#   end
-#
-#   rounded_x_squared = rounded_x.map do |num|
-#     num**2
-#   end
-#
-#   std_dev = Math.sqrt(rounded_x_squared.sum/total)
-#   p std_dev.round(2)
-# end
-#
-# std_dev(ages)
-
-
-
-
 def std_dev(ages)
   sum = ages.sum.to_f
   total = ages.count.to_f
   average = sum/total
 
-  
+  x = ages.map do |age|
+    age - average
+  end
+
+  rounded_x = x.map do |num|
+    num.round(2)
+  end
 
   rounded_x_squared = rounded_x.map do |num|
     num**2
@@ -44,19 +22,5 @@ def std_dev(ages)
   std_dev = Math.sqrt(rounded_x_squared.sum/total)
   p std_dev.round(2)
 end
-
-
-def
-  x = ages.map do |age|
-    age - average
-  end
-
-  rounded_x = x.map do |num|
-    num.round(2)
-  end
-end
-
-
-
 
 std_dev(ages)
